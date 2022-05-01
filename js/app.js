e13919e10d7efebe760189c8f4fd9429b223eaa8
gerizzo <gerardorizzo99@gmail.com>
2022-04-30 23:10:02 -0300

    NUEVA IMAGEN DE CARROUSEL y PRIMER AVANCE EN PRODUCTOS DESTACADOS POR JAVASCRIPT

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+/* PRODUCTOS DESTACADOS EN EL HOME */
+
+/* let expositorIndex = document.querySelector(".expositorProductosDestacados");
+let contenedorProductoDestacado = document.querySelector("div");
+productosDestacados.forEach(element => {
+  contenedorProductoDestacado.classList.add("d-flex", "justify-content-center", "col-sm-12", "col-md-6 col-lg-4 col-xl-3")
+  contenedorProductoDestacado.innerHTML =
+    `
+    <div class="card cardProductos" style="width: 18rem;">
+        <img src="${element.img}" class="card-img-top" alt="...">
+      <div class="card-body text-center">
+        <h5 class="card-title nombreProducto text-center">${element.nombre}</h5>
+        <p class="card-text textoExpositor">Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Porro
+          mollitia ratione sed quo ut non aspernatur vero vitae praesentium est.</p>
+        <p class="card-text precioProducto text-center">$${element.precio}</p>
+        <a href="#" class="btn btn-secondary btnComprar botonColorVioleta">Comprar</a>
+      </div>
+    </div>
+  `
+  expositorIndex.appendChild(contenedorProductoDestacado)
+})
+ */
+
 // evento secccion productos
 navProductos.addEventListener("click", seccionProductos)
 // funcion productos
@@ -8,16 +32,17 @@ function seccionProductos() {
   main.textContent = ""
   main.innerHTML = `<h1 class="titleProductos">Productos</h1>`
   productos.forEach(element => {
-    stockProductos.innerHTML += `
-        <div class="shadow card cardProductos" id="${element.id}" style="width: 18rem;">
-          <img src="${element.img}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <span style="display: none" class="idProducto">${element.id}</span>
-            <h5 class="card-title nombreProducto">${element.nombre}</h5>
-            <p class="card-text precioProducto">$${element.precio}</p>
-            <a href="#" class="btn btn-secondary btnComprar botonColorVioleta">Comprar</a>
+    stockProductos.innerHTML +=
+      `
+          <div class="shadow card cardProductos" id="${element.id}" style="width: 18rem;">
+            <img src="${element.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <span style="display: none" class="idProducto">${element.id}</span>
+              <h5 class="card-title nombreProducto text-center">${element.nombre}</h5>
+              <p class="card-text precioProducto">$${element.precio}</p>
+              <a href="#" class="btn btn-secondary btnComprar botonColorVioleta">Comprar</a>
+            </div>
           </div>
-        </div>
         `
     main.appendChild(stockProductos)
     capturarBotones()
