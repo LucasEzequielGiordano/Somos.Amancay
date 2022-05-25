921e915d604ed7fe2ce84a5e2cd373c03bccf9a3
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-05-25 18:54:55 -0300

    arreglos footer y detalles en secciones

--- js/botiquin.js
+++ js/botiquin.js
@@ -1,6 +1,15 @@
 const recursero = () => {
     main.innerHTML = ''
-
+    main.className = 'mainBotiquin'
+    const botiquinEmocional = document.createElement('h2')
+    botiquinEmocional.className = 'botiquinEmocional'
+    botiquinEmocional.innerHTML = 'Botiquin Emocional'
+    main.appendChild(botiquinEmocional)
+    const divBotiquin = document.createElement('div')
+    divBotiquin.className = 'divBotiquin'
+    divBotiquin.innerHTML = `
+    
+    `
 }
 
 const navBotiquin = document.getElementById('navBotiquin').addEventListener('click', recursero)
